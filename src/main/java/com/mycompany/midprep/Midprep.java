24af2d81fe32ce6fc705efb1b335962c3923cbc8
harneet2003 <kau16367@sheridancollege.ca>
2024-06-16 20:48:25 -0400

    mid term

---
+++ src/main/java/com/mycompany/midprep/Midprep.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.midprep;
+
+/**
+ *
+ * @author User
+ */
+public class Midprep {
+
+    public static void main(String[] args) {
+        
+       
+        System.out.println("Hello World!");
+    }
+}
