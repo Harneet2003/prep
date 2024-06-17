1ccc555b3250966c7cd71e94ad0fb1b84aac2057
harneet2003 <kau16367@sheridancollege.ca>
2024-06-16 20:53:21 -0400

    Revert "mid term"
    This reverts commit 242a21b1b6c0d07811c68dd38af8ed34d0d7e82b.

--- src/main/java/com/mycompany/midprep/Midprep.java
+++ src/main/java/com/mycompany/midprep/Midprep.java
@@ -12,7 +12,7 @@ public class Midprep {
 
     public static void main(String[] args) {
         
-       System.out.println("Midterm prep");
+       
         System.out.println("Hello World!");
     }
 }
